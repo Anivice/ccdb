f0ef8dc195de018cd2fca27c1c6b97fbeb26270d
anivice <AniviceIves@gmail.com>
2025-12-02 23:22:17 +0800

    MUSL testing

--- python_snips/proxy.py
+++
@@ -1,6 +0,0 @@
-# Fetch all proxies and groups
-resp = requests.get(f"{base_url}/proxies", headers=headers)
-proxies_info = resp.json()
-# proxies_info will be a dict with proxy names and their info (type, history, etc.)
-for name, info in proxies_info.get("proxies", {}).items():
-    print(f"Proxy: {name}, Type: {info.get('type')}, Now: {info.get('now')}")
