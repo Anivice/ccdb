26e0ec63259eed23f9461607d116f038182a8448
anivice <AniviceIves@gmail.com>
2026-01-13 21:13:13 +0800

    CYGWIN build effort

--- src/misc/utils.cpp
+++ src/misc/utils.cpp
@@ -235,12 +235,13 @@ int ccdb::utils::UnicodeDisplayWidth::get_width_utf32(const std::u32string &utf3
 
 int ccdb::utils::UnicodeDisplayWidth::get_char_width(const char32_t c)
 {
+#ifndef _CCDB_CYGWIN_BUILD_
     const auto wc = static_cast<wchar_t>(c);
 
     if (const int w = wcwidth(wc); w >= 0) {
         return w;
     }
-
+#endif
     return fallback_char_width(c);
 }
 
