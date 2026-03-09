2b0b3a0254b5549a96295d3fb73523ab9a3c59dd
anivice <AniviceIves@gmail.com>
2026-03-09 15:44:02 +0800

    .

--- src/configure.sh
+++ src/configure.sh
@@ -82,6 +82,6 @@ env PATH="$MUSL_SYSROOT"/bin/:"$PATH" cmake -B "$BUILD_DIR" -S "$script_dir" \
 pushd "$PWD"
 cd "$BUILD_DIR"
 env PATH="$MUSL_SYSROOT"/bin/:"$PATH" make CFLAGS="$CMAKE_CFLAGS" CXXFLAGS="$CMAKE_CFLAGS" -j"$(nproc)"
-env PATH="$MUSL_SYSROOT"/bin/:"$PATH" cp ccdb ccdb.debug_info
-$STRIP ccdb
+cp ccdb ccdb.debug_info
+env PATH="$MUSL_SYSROOT"/bin/:"$PATH" $STRIP ccdb
 popd
