f09e14c1950a590f4020273a3359009848b9ba8d
Anivice Ives <aniviceives@gmail.com>
2026-05-21 22:28:58 +0800

    simplified time to human readable

--- src/misc/utils.cpp
+++ src/misc/utils.cpp
@@ -784,11 +784,7 @@ std::string ccdb::utils::second_to_human_readable(unsigned long long value)
 
     const unsigned long long day = value / (60 * 60 * 24);
     value %= (60 * 60 * 24);
-    const unsigned long long hour = value / (60 * 60);
-    value %= (60 * 60);
-    const unsigned long long minute = value / 60;
-    const unsigned long long second = value % 60;
-    return std::to_string(day) + "d " + std::to_string(hour) + "h " + std::to_string(minute) + "m " + std::to_string(second) + "s";
+    return std::to_string(day) + "d " + second_to_human_readable(value);
 }
 
 std::u32string ccdb::utils::utf8_to_u32(const std::string &s)
