76b1eb091e42658b0a24585307635d4c55b18f2c
Anivice Ives <aniviceives@gmail.com>
2026-07-24 21:17:57 +0800

    build fix

--- src/misc/update.cpp
+++ src/misc/update.cpp
@@ -14,7 +14,10 @@ static constexpr const char * getBuild()
     return "x86_64";
 #elif defined(i386) || defined(__i386__) || defined(__i386) || defined(_M_IX86)
     return "i586";
-#elif defined(__ARM_ARCH_7__) || defined(__ARM_ARCH_7A__) || defined(__ARM_ARCH_7R__) || defined(__ARM_ARCH_7M__) || defined(__ARM_ARCH_7S__) || defined(__ARM_ARCH_7A__) || defined(__ARM_ARCH_7R__) || defined(__ARM_ARCH_7M__) || defined(__ARM_ARCH_7S__) || defined(__ARM_ARCH_7R__) || defined(__ARM_ARCH_7M__) || defined(__ARM_ARCH_7S__) defined(__ARM_ARCH_7M__) || defined(__ARM_ARCH_7S__)
+#elif defined(__ARM_ARCH_7__) || defined(__ARM_ARCH_7A__) || defined(__ARM_ARCH_7R__) || defined(__ARM_ARCH_7M__) \
+    || defined(__ARM_ARCH_7S__) || defined(__ARM_ARCH_7A__) || defined(__ARM_ARCH_7R__) || defined(__ARM_ARCH_7M__) \
+    || defined(__ARM_ARCH_7S__) || defined(__ARM_ARCH_7R__) || defined(__ARM_ARCH_7M__) || defined(__ARM_ARCH_7S__) \
+    || defined(__ARM_ARCH_7M__) || defined(__ARM_ARCH_7S__)
     return "armv7";
 #elif defined(__aarch64__) || defined(_M_ARM64)
     return "aarch64";
