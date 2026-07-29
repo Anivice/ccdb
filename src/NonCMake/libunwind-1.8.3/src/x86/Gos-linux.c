d14a6d60a3ae2bd3e413f4c952d4183d30d1269e
anivice <aniviceives@gmail.com>
2026-07-29 15:05:12 +0800

    emergency tracer

--- src/NonCMake/libunwind-1.8.3/src/x86/Gos-linux.c
+++ src/NonCMake/libunwind-1.8.3/src/x86/Gos-linux.c
@@ -293,7 +293,8 @@ x86_local_resume (unw_addr_space_t as, unw_cursor_t *cursor, void *arg)
 
   Debug (8, "resuming at ip=%x via setcontext()\n", c->dwarf.ip);
 #if !defined(__ANDROID__)
-  setcontext (uc);
+  // setcontext (uc);
+  abort(); // WARNING! DO NOT CALL RESUME!!!
 #endif
   return -UNW_EINVAL;
 }
