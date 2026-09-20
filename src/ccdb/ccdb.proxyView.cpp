4f184423744aa0d532288476584fe77865856b74
anivice <aniviceives@gmail.com>
2026-09-20 16:03:02 +0800

    timeline

--- src/ccdb/ccdb.proxyView.cpp
+++ src/ccdb/ccdb.proxyView.cpp
@@ -39,27 +39,30 @@ namespace
         std::chrono::time_point<std::chrono::steady_clock> last_frame_time;
         int leading_space{};
         int skip_lines{};
+        int width{};
+        int height{};
+        int mouse_x{};
+        int mouse_y{};
     };
 }
 
-static void proxyView_peak(std::vector<std::string> & frame,
-    int & width, int & height, const int & mouse_x, const int & mouse_y,
-    cross_frame_context_t & cross_frame_context)
+static void proxyView_peak(std::vector<std::string> & frame, cross_frame_context_t & cross_frame_context)
 {
     const auto last_frame_time_backup = cross_frame_context.last_frame_time;
     cross_frame_context.last_frame_time = std::chrono::steady_clock::now();
-    width = 256; height = 256;
-    frame.resize(height, std::string(width, '*'));
-    if (mouse_x != -1 && mouse_y != -1) {
+    cross_frame_context.width = 256; cross_frame_context.height = 256;
+    frame.resize(cross_frame_context.height, std::string(cross_frame_context.width, ' '));
+    if (cross_frame_context.mouse_x != -1 && cross_frame_context.mouse_y != -1)
+    {
         cross_frame_context.mouse_position = {
-            mouse_x + cross_frame_context.leading_space - 1 /* starts with 0 */ - 1 /* row indicator */
+            cross_frame_context.mouse_x + cross_frame_context.leading_space - 1 /* starts with 0 */ - 1 /* row indicator */
                 - (cross_frame_context.leading_space > 0 ? 1 : 0),
-            mouse_y + cross_frame_context.skip_lines - 1
+            cross_frame_context.mouse_y + cross_frame_context.skip_lines - 1
         };
 
-        if (cross_frame_context.mouse_position.first < 0 || cross_frame_context.mouse_position.first >= width)
+        if (cross_frame_context.mouse_position.first < 0 || cross_frame_context.mouse_position.first >= cross_frame_context.width)
             cross_frame_context.mouse_position.first = -1;
-        if (cross_frame_context.mouse_position.second < 0 || cross_frame_context.mouse_position.second >= height)
+        if (cross_frame_context.mouse_position.second < 0 || cross_frame_context.mouse_position.second >= cross_frame_context.height)
             cross_frame_context.mouse_position.second = -1;
     }
 
@@ -72,6 +75,10 @@ static void proxyView_peak(std::vector<std::string> & frame,
     const std::string FPS_indicator = FPS_indicator_ss.str();
     for (uint64_t i = 0; i < FPS_indicator.size(); ++i)
         frame[0][i] = FPS_indicator[i];
+
+    {
+
+    }
 }
 
 void ccdb::ccdb::proxyView()
@@ -82,7 +89,6 @@ void ccdb::ccdb::proxyView()
     ccdb_atomic_t<frame_data_t> frame_data;
     std::atomic_int row_ = utils::get_line_size(), col_ = utils::get_col_size(),
         mouse_x_ = -1, mouse_y_ = -1, leading_space_ = 0, max_leading_space_ = 0, skip_lines_ = 0, max_skip_lines_ = 0;
-    int content_view_width = 0, content_view_height = 0;
     cross_frame_context_t cross_frame_context;
     std::atomic_bool window_size_changed = false;
     auto watcher_ = watcher.make_status_watcher();
@@ -126,11 +132,14 @@ void ccdb::ccdb::proxyView()
         std::vector<std::string> vector_frame_view;
         cross_frame_context.leading_space = leading_space;
         cross_frame_context.skip_lines = skip_lines;
-        proxyView_peak(vector_frame_view, content_view_width, content_view_height, mouse_x, mouse_y, cross_frame_context);
+        cross_frame_context.mouse_x = mouse_x;
+        cross_frame_context.mouse_y = mouse_y;
+
+        proxyView_peak(vector_frame_view, cross_frame_context);
 
         // recalibrate boundaries for this frame
-        max_leading_space_ = content_view_width > col_ - 1 ? content_view_width - col_ - 1 : 0;
-        max_skip_lines_ = content_view_height > row_ - 1 ? content_view_height - row_ - 1 : 0;
+        max_leading_space_ = cross_frame_context.width > col_ - 1 ? cross_frame_context.width - col_ - 1 : 0;
+        max_skip_lines_ = cross_frame_context.height > row_ - 1 ? cross_frame_context.height - row_ - 1 : 0;
 
         if (leading_space_ > max_leading_space_) leading_space_ = max_leading_space_.load();
         if (skip_lines_ > max_skip_lines_) skip_lines_ = max_skip_lines_.load();
@@ -138,8 +147,25 @@ void ccdb::ccdb::proxyView()
         const int row = row_;
         const int col = col_;
 
-        const int max_leading_space = max_leading_space_;
-        const int max_skip_lines = max_skip_lines_;
+        if (mouse_y == row && mouse_x >= 1)
+        {
+            const int ll_max_leading_spaces_ = max_leading_space_;
+            auto new_leading_space = static_cast<int>(std::round(static_cast<double>(mouse_x) /
+                static_cast<double>(col) * ll_max_leading_spaces_));
+            if (new_leading_space > ll_max_leading_spaces_) new_leading_space = ll_max_leading_spaces_;
+            if (new_leading_space < 0) new_leading_space = 0;
+            leading_space_ = new_leading_space;
+        }
+
+        if (mouse_x == 1)
+        {
+            const int ll_max_skip_lines = max_skip_lines_;
+            auto new_skip_lines = mouse_y == 1 ? 0 :
+                static_cast<int>(std::round(static_cast<double>(mouse_y) / static_cast<double>(row - 1) * ll_max_skip_lines));
+            if (new_skip_lines > ll_max_skip_lines) new_skip_lines = ll_max_skip_lines;
+            if (new_skip_lines < 0) new_skip_lines = 0;
+            skip_lines_ = new_skip_lines;
+        }
 
         const auto now = std::chrono::steady_clock::now();
         if (window_size_changed.load(std::memory_order_relaxed))
@@ -169,13 +195,14 @@ void ccdb::ccdb::proxyView()
                     presumed_view_point_end > vector_frame_view.size() ? vector_frame_view.size() : presumed_view_point_end),
             };
 
-            const auto width_strip = utils::generate_linear_handle(content_view_width,
-                leading_space, leading_space + col, col);
-            const auto height_strip = utf8::utf8to32(utils::generate_linear_handle(content_view_height,
+            const auto width_strip = utils::generate_linear_handle(cross_frame_context.width,
+                leading_space, leading_space + viewSize_col, viewSize_col);
+            const auto height_strip = utf8::utf8to32(utils::generate_linear_handle(cross_frame_context.height,
                 skip_lines, skip_lines + viewSize_row, viewSize_row));
 
             int offset = 0;
-            for (const auto & view : vector_frame_view) {
+            for (const auto & view : vector_frame_view)
+            {
                 frame << utf8::utf32to8({height_strip[offset++]});
                 std::u32string u32 = utf8::utf8to32(view);
                 int printed_width = 0, skipped_width = 0;
